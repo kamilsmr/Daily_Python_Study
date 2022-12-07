9a914f9abb06683faa9810c013ea1ebb3c5175fc
kamilsmr <kamils6035@gmail.com>
2022-12-07 09:59:51 +0300

    string

---
+++ string.py
@@ -0,0 +1,35 @@
+
+'hello'
+"hello"
+"I'm a kamil"
+
+mystring = 'abcdefg'
+print(mystring)
+print(mystring[0])
+print(mystring[-1])
+print(mystring[2:])
+print(mystring[4:])
+print(mystring[:3])
+print(mystring[2:5])
+print(mystring[::1])
+print(mystring[::2])
+mystring = 'x'
+
+mystring.upper()
+x = mystring.lower()
+x = mystring.capitalize
+x = mystring.split()
+x = mystring.split('e')
+
+# .format
+
+x = "insert another string here: {}".format("INSERT ME!")
+print(x)
+x = "Item One: {} Item Two:{}".format("dog","cat")
+print(x)
+
+x = "Item One: {a} Item Two: {b}".format(x="dog",y="cat")
+print(x)
+
+
+
